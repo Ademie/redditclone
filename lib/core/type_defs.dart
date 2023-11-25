9ee5a49fed43e67d49afbdc5a0bd109566bdb791
ademie <isijolaademie@gmail.com>
2023-11-25 11:21:19 -0600

    using fpdart for advanced error handling

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -1,6 +1,7 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:redditclone/core/failure.dart';
 
-
+// success can be anythin e.g T
+// left l failure, right r success
 typedef FutureEither<T> = Future<Either<Failure, T>>;
 typedef FutureVoid = FutureEither<void>;
